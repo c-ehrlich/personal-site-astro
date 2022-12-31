5c2d15791219833f5fd5121ba5fefca02fd5534c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-31 15:53:32 +0900

    initial page studs

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,15 +1,21 @@
 import { createEffect, createSignal, JSX } from "solid-js";
 import { Motion, Presence } from "@motionone/solid";
 
-export function TestSolid() {
+export function ThemeToggle() {
   const [theme, setTheme] = createSignal(localStorage.getItem("theme"));
 
   createEffect(() => {
-    localStorage.setItem("theme", theme() || "dark");
-    if (theme() === "light") {
-      document.documentElement.classList.remove("dark");
+    console.log("running createEffect", theme());
+    if (theme()) {
+      console.log("setting theme");
+      localStorage.setItem("theme", theme() || "dark");
+      if (theme() === "light") {
+        document.documentElement.classList.remove("dark");
+      } else {
+        document.documentElement.classList.add("dark");
+      }
     } else {
-      document.documentElement.classList.add("dark");
+      setTheme("dark");
     }
   });
 
@@ -19,11 +25,12 @@ export function TestSolid() {
 
   return (
     <div>
+      <p class="text-red-500">{theme()}</p>
       <Presence exitBeforeEnter>
-        {theme() === "dark" ? (
-          <ThemeButton handleClick={handleSwitchTheme}>🌙</ThemeButton>
-        ) : (
+        {theme() === "light" ? (
           <ThemeButton handleClick={handleSwitchTheme}>🌤️</ThemeButton>
+        ) : (
+          <ThemeButton handleClick={handleSwitchTheme}>🌙</ThemeButton>
         )}
       </Presence>
     </div>
